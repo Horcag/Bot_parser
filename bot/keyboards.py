0ff375b1ce8edf4fdd79dfdc580686b1d07a088d
Nikita <94986737+Horcag@users.noreply.github.com>
2023-07-08 12:21:35 +0400

    Обновление.
    Были добавлены:
    кнопки и их обработчик;
    выбор направления и СНИЛСа;
    различные незначительные улучшения;

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,15 +1,18 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, inline_keyboard, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
-ik: inline_keyboard.InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[
-    InlineKeyboardButton(text='Прикладная математика и информатика', callback_data='2'),
-    InlineKeyboardButton(text='Фундаментальные информатика и информационные технологии'),
-    InlineKeyboardButton(text='Прикладные математика и физика'),
-    InlineKeyboardButton(text='Информатика и вычислительная техника'),
-    InlineKeyboardButton(text='Информационная безопасность автоматизированных систем'),
-    InlineKeyboardButton(text='Конструирование и технология электронных средств'),
-    InlineKeyboardButton(text='Электроника и наноэлектроника'),
-    InlineKeyboardButton(text='Радиоэлектронные системы и комплексы'),
-    InlineKeyboardButton(text='Фотоника и оптоинформатика'),
-    InlineKeyboardButton(text='Биотехнические системы и технологии'),
-    InlineKeyboardButton(text='Лазерная техника и лазерные технологии')
-], row_width=2)
+
+def get_directions_keyboard() -> InlineKeyboardMarkup:
+    ik: inline_keyboard.InlineKeyboardMarkup = InlineKeyboardMarkup(row_width=1)
+    ik.add(InlineKeyboardButton(text='Прикладная математика и информатика', callback_data='2'),
+           InlineKeyboardButton(text='Фундаментальные информатика и информационные технологии', callback_data='7'),
+           InlineKeyboardButton(text='Прикладные математика и физика', callback_data='9'),
+           InlineKeyboardButton(text='Информатика и вычислительная техника', callback_data='14'),
+           InlineKeyboardButton(text='Информационная безопасность автоматизированных систем', callback_data='17'),
+           InlineKeyboardButton(text='Конструирование и технология электронных средств', callback_data='18'),
+           InlineKeyboardButton(text='Электроника и наноэлектроника', callback_data='19'),
+           InlineKeyboardButton(text='Радиоэлектронные системы и комплексы', callback_data='20'),
+           InlineKeyboardButton(text='Фотоника и оптоинформатика', callback_data='21'),
+           InlineKeyboardButton(text='Биотехнические системы и технологии', callback_data='22'),
+           InlineKeyboardButton(text='Лазерная техника и лазерные технологии', callback_data='23')
+           )
+    return ik
--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,27 @@
 from re import compile, Pattern, match
 from config_reader import config
 from aiogram import Bot, Dispatcher, executor, types
-from keyboards import ik
+from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import keyboards
 import parser
 
 storage: MemoryStorage = MemoryStorage()
 bot: Bot = Bot(token=config.bot_token.get_secret_value())
-dp: Dispatcher = Dispatcher(bot=bot,
-                            storage=storage)
-snl_pattern: Pattern = compile(r'^\d{3}-\d{3}-\d{3}\s\d{2}$')
+dp: Dispatcher = Dispatcher(bot=bot, storage=storage)
+snl_pattern: Pattern = compile(r'^\d{3}-\d{3}-\d{3}-\d{2}$')
+HELP: str = '''
+Команды бота:
+/снилс - ввод своего СНИЛСа
+/направления - выбор своего направления
+'''
+TEXT_DIRECTIONS: str = 'направление, где у вас высший приоритет. НЕ РАБОТАЕТ С ИНЫМИ ПРИОРИТЕТАМИ.'
+
+
+class UserState(StatesGroup):
+    SNILS: State = State()
+    SELECT_DIRECTION: State = State()
 
 
 async def on_startup(_):
@@ -19,33 +31,51 @@ async def on_startup(_):
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
-                           text='Пришлите свой СНИЛС в формате 123-456-789-00')
+                           text='Здравствуйте! Чтобы начать пользоваться нужно указать свой СНИЛС и направление. Напишите команду /снилс')
     await message.delete()
 
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
-                           text='Напишите /start, чтобы использовать бота.')
+                           text=HELP)
     await message.delete()
+
+
+@dp.message_handler(commands=['снилс'])
+async def get_snl(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Пожалуйста, отправьте свой СНИЛС в формате 123-456-789-00')
+    await UserState.SNILS.set()
 
 
 @dp.message_handler(commands=['направления'])
 async def get_direction_list(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
-                           text='Выберите направление, где у вас высший приоритет. НЕ РАБОТАЕТ С ИНЫМИ ПРИОРИТЕТАМИ.',
-                           reply_markup=ik)
+                           text=f'Выберите {TEXT_DIRECTIONS}',
+                           reply_markup=keyboards.get_directions_keyboard())
+    await UserState.SELECT_DIRECTION.set()
 
 
-@dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def process_snl(message: types.Message):
+@dp.message_handler(state=UserState.SNILS)
+async def process_snl(message: types.Message, state=FSMContext):
     snl: str = message.text.strip()  # Получаем переданный СНИЛС и удаляем лишние пробелы
 
     if match(snl_pattern, snl):
-        save_snl_to_database(snl)
-        await message.reply("СНИЛС верный! Он сохранен.")
+        await state.update_data(snl=snl)
+        await message.reply(f"СНИЛС верный! Теперь выберете {TEXT_DIRECTIONS}.",
+                            reply_markup=keyboards.get_directions_keyboard())
+        await UserState.SELECT_DIRECTION.set()
     else:
         await message.reply("Неверный СНИЛС, попробуйте снова.")
+
+
+@dp.callback_query_handler(state=UserState.SELECT_DIRECTION)
+async def process_direction(callback_query: types.CallbackQuery, state: FSMContext):
+    direction = callback_query.data  # Получаем выбранное направление
+    await callback_query.answer()  # Отправляем подтверждение нажатия кнопки
+    await callback_query.message.reply(f"Вы выбрали направление: {direction}")
+    await state.finish()
 
 
 def save_snl_to_database(snl: str):
@@ -53,6 +83,4 @@ def save_snl_to_database(snl: str):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dispatcher=dp,
-                           skip_updates=True,
-                           on_startup=on_startup)
+    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=on_startup)
