0bf960185911f64b1b59fdc8cc2f8af1f771de08
Nikita <94986737+Horcag@users.noreply.github.com>
2023-07-08 16:48:44 +0400

    Новая логика.
    Полностью переработан интерфейс взаимодействия с пользователем.
    Теперь бот изменяет свои сообщения, а не отправляет новые.
    Добавлено подтверждение выбора направления.
    + мелкие фиксы

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,18 +1,47 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, inline_keyboard, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
+all_directions_dictionary: dict[str, str] = {
+    '2': 'Прикладная математика и информатика',
+    '7': 'Фундаментальные информатика и информационные технологии',
+    '9': 'Прикладные математика и физика',
+    '14': 'Информатика и вычислительная техника',
+    '17': 'Информационная безопасность автоматизированных систем',
+    '18': 'Конструирование и технология электронных средств',
+    '19': 'Электроника и наноэлектроника',
+    '20': 'Радиоэлектронные системы и комплексы',
+    '21': 'Фотоника и оптоинформатика',
+    '22': 'Биотехнические системы и технологии',
+    '23': 'Лазерная техника и лазерные технологии'
+
+}
+
 
 def get_directions_keyboard() -> InlineKeyboardMarkup:
-    ik: inline_keyboard.InlineKeyboardMarkup = InlineKeyboardMarkup(row_width=1)
-    ik.add(InlineKeyboardButton(text='Прикладная математика и информатика', callback_data='2'),
-           InlineKeyboardButton(text='Фундаментальные информатика и информационные технологии', callback_data='7'),
-           InlineKeyboardButton(text='Прикладные математика и физика', callback_data='9'),
-           InlineKeyboardButton(text='Информатика и вычислительная техника', callback_data='14'),
-           InlineKeyboardButton(text='Информационная безопасность автоматизированных систем', callback_data='17'),
-           InlineKeyboardButton(text='Конструирование и технология электронных средств', callback_data='18'),
-           InlineKeyboardButton(text='Электроника и наноэлектроника', callback_data='19'),
-           InlineKeyboardButton(text='Радиоэлектронные системы и комплексы', callback_data='20'),
-           InlineKeyboardButton(text='Фотоника и оптоинформатика', callback_data='21'),
-           InlineKeyboardButton(text='Биотехнические системы и технологии', callback_data='22'),
-           InlineKeyboardButton(text='Лазерная техника и лазерные технологии', callback_data='23')
+    ik: InlineKeyboardMarkup = InlineKeyboardMarkup(row_width=1)
+    ik.add(InlineKeyboardButton(text=all_directions_dictionary['2'], callback_data='2'),
+           InlineKeyboardButton(text=all_directions_dictionary['7'], callback_data='7'),
+           InlineKeyboardButton(text=all_directions_dictionary['9'], callback_data='9'),
+           InlineKeyboardButton(text=all_directions_dictionary['14'], callback_data='14'),
+           InlineKeyboardButton(text=all_directions_dictionary['17'], callback_data='17'),
+           InlineKeyboardButton(text=all_directions_dictionary['18'], callback_data='18'),
+           InlineKeyboardButton(text=all_directions_dictionary['19'], callback_data='19'),
+           InlineKeyboardButton(text=all_directions_dictionary['20'], callback_data='20'),
+           InlineKeyboardButton(text=all_directions_dictionary['21'], callback_data='21'),
+           InlineKeyboardButton(text=all_directions_dictionary['22'], callback_data='22'),
+           InlineKeyboardButton(text=all_directions_dictionary['23'], callback_data='23')
            )
+    return ik
+
+
+def confirmation_of_selection() -> InlineKeyboardMarkup:
+    ik: InlineKeyboardMarkup = InlineKeyboardMarkup(row_width=2)
+    ik.add(InlineKeyboardButton(text='Подтвердить ✅', callback_data='yes'),
+           InlineKeyboardButton(text='Отменить ❌', callback_data='no')
+           )
+    return ik
+
+
+def command_snl() -> InlineKeyboardMarkup:
+    ik: InlineKeyboardMarkup = InlineKeyboardMarkup(row_width=1)
+    ik.add(InlineKeyboardButton(text='Ввести свой снилс:', callback_data='enter_snl'))
     return ik
--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,13 @@
 from re import compile, Pattern, match
 from config_reader import config
+import keyboards
+import parser
+import typing
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
-import keyboards
-import parser
 
 storage: MemoryStorage = MemoryStorage()
 bot: Bot = Bot(token=config.bot_token.get_secret_value())
@@ -13,69 +15,113 @@ dp: Dispatcher = Dispatcher(bot=bot, storage=storage)
 snl_pattern: Pattern = compile(r'^\d{3}-\d{3}-\d{3}-\d{2}$')
 HELP: str = '''
 Команды бота:
-/снилс - ввод своего СНИЛСа
-/направления - выбор своего направления
+/snils - ввод своего СНИЛСа
+/directions - выбор своего направления
 '''
-TEXT_DIRECTIONS: str = 'направление, где у вас высший приоритет. НЕ РАБОТАЕТ С ИНЫМИ ПРИОРИТЕТАМИ.'
+TEXT_DIRECTIONS: str = 'направление, где у вас высший приоритет. <b>НЕ РАБОТАЕТ С ИНЫМИ ПРИОРИТЕТАМИ</b>'
+TEXT_SNL: str = 'Пожалуйста, отправьте свой СНИЛС в формате <b>123-456-789-00</b>'
 
 
 class UserState(StatesGroup):
     SNILS: State = State()
     SELECT_DIRECTION: State = State()
+    YES_OR_NO_SELECT_DIRECTION: State = State()
 
 
-async def on_startup(_):
+async def on_startup(_) -> None:
     print('Бот запущен.')
 
 
 @dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
+async def start_command(message: types.Message) -> None:
     await bot.send_message(chat_id=message.from_user.id,
-                           text='Здравствуйте! Чтобы начать пользоваться нужно указать свой СНИЛС и направление. Напишите команду /снилс')
-    await message.delete()
+                           text='Здравствуйте! Чтобы начать пользоваться ботом нужно указать свой <b>СНИЛС</b> и <b>направление</b>.',
+                           reply_markup=keyboards.command_snl(),
+                           parse_mode='HTML')
+    # await message.delete()
 
 
 @dp.message_handler(commands=['help'])
-async def help_command(message: types.Message):
+async def help_command(message: types.Message) -> None:
     await bot.send_message(chat_id=message.from_user.id,
                            text=HELP)
-    await message.delete()
+    # await message.delete()
 
 
-@dp.message_handler(commands=['снилс'])
-async def get_snl(message: types.Message):
+@dp.message_handler(commands=['snils'])
+async def get_snl(message: types.Message) -> None:
     await bot.send_message(chat_id=message.from_user.id,
-                           text='Пожалуйста, отправьте свой СНИЛС в формате 123-456-789-00')
+                           text=TEXT_SNL,
+                           parse_mode='HTML')
     await UserState.SNILS.set()
 
 
-@dp.message_handler(commands=['направления'])
-async def get_direction_list(message: types.Message):
+@dp.message_handler(commands=['directions'])
+async def get_direction_list(message: types.Message) -> None:
     await bot.send_message(chat_id=message.from_user.id,
                            text=f'Выберите {TEXT_DIRECTIONS}',
-                           reply_markup=keyboards.get_directions_keyboard())
+                           reply_markup=keyboards.get_directions_keyboard(),
+                           parse_mode='HTML')
     await UserState.SELECT_DIRECTION.set()
 
 
 @dp.message_handler(state=UserState.SNILS)
-async def process_snl(message: types.Message, state=FSMContext):
+async def process_of_getting_snl(message: types.Message, state=FSMContext) -> None:
     snl: str = message.text.strip()  # Получаем переданный СНИЛС и удаляем лишние пробелы
 
     if match(snl_pattern, snl):
-        await state.update_data(snl=snl)
-        await message.reply(f"СНИЛС верный! Теперь выберете {TEXT_DIRECTIONS}.",
-                            reply_markup=keyboards.get_directions_keyboard())
-        await UserState.SELECT_DIRECTION.set()
+        async with state.proxy() as data:
+            print(data)
+            data['snl'] = snl
+            print(data)
+        if data.get('direction') is None:
+            await message.answer(text=f'СНИЛС введен корректно! Теперь выберете {TEXT_DIRECTIONS}.',
+                                 reply_markup=keyboards.get_directions_keyboard(),
+                                 parse_mode='HTML')
+            await UserState.SELECT_DIRECTION.set()
+        else:
+            await message.answer(text=f'СНИЛС сохранен!')
+            await state.finish()
     else:
-        await message.reply("Неверный СНИЛС, попробуйте снова.")
+        await message.answer('Неверный СНИЛС, попробуйте снова.')
 
 
 @dp.callback_query_handler(state=UserState.SELECT_DIRECTION)
-async def process_direction(callback_query: types.CallbackQuery, state: FSMContext):
+async def destination_selection_process(callback_query: types.CallbackQuery, state: FSMContext) -> None:
     direction = callback_query.data  # Получаем выбранное направление
     await callback_query.answer()  # Отправляем подтверждение нажатия кнопки
-    await callback_query.message.reply(f"Вы выбрали направление: {direction}")
-    await state.finish()
+    async with state.proxy() as data:
+        print(data)
+        data['direction'] = keyboards.all_directions_dictionary[direction]
+        print(data)
+    await callback_query.message.edit_text(text=f'Вы выбрали направление: <b>{keyboards.all_directions_dictionary[direction]}</b>',
+                                           reply_markup=keyboards.confirmation_of_selection(),
+                                           parse_mode='HTML')
+    await UserState.YES_OR_NO_SELECT_DIRECTION.set()
+
+
+@dp.callback_query_handler(state=UserState.YES_OR_NO_SELECT_DIRECTION)
+async def confirmation_process(callback_query: types.CallbackQuery, state: FSMContext) -> None:
+    answer = callback_query.data
+    await callback_query.answer()
+    match answer:
+        case 'no':
+            await callback_query.message.edit_text(text=f'Выберите {TEXT_DIRECTIONS}',
+                                                   reply_markup=keyboards.get_directions_keyboard(),
+                                                   parse_mode='HTML')
+            await UserState.SELECT_DIRECTION.set()
+        case 'yes':
+            await callback_query.message.edit_text('Направление сохранено!')
+            await state.finish()
+        case _:
+            raise 'Неизвестная команда'
+
+
+@dp.callback_query_handler(lambda callback_query: callback_query.data == "enter_snl")
+async def enter_snils(callback_query: types.CallbackQuery, state: FSMContext) -> None:
+    await callback_query.message.edit_text(text=TEXT_SNL,
+                                           parse_mode='HTML')
+    await UserState.SNILS.set()
 
 
 def save_snl_to_database(snl: str):
